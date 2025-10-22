a7e83c2cdf7b01c5f6e3e546039651f8502bb24f
T2xpdmVy <179873@cloud.rangitoto.school.nz>
2025-10-22 15:09:24 +1300

    I made a function to move the servo without writing a for loop every time

---
+++ electronics_light_sensor/electronics_light_sensor.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+void move_to(int degree, int wait = 15) { // new function to add delay and move the panel without need to write a for loop every time
+  int change_in_degrees = degree - myservo.read();
+  for (int pos = 0; pos <= change_in_degrees; pos +=change_in_degrees/abs(change_in_degrees) ) {
+    myservo.write(pos);
+    delay(wait);
+  }
+}
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+}
+
+
+void loop() {
+  move_to(90);
+  move_to(180);
+  move_to(270);
+  move_to(360);
+  move_to(0);
+}
