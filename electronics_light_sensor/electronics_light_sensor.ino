79806be4631ea4ecfdc7c43bb07423c4dd032f93
T2xpdmVy <179873@cloud.rangitoto.school.nz>
2025-10-24 13:20:50 +1300

    fixed some numbers maybe

--- electronics_light_sensor/electronics_light_sensor.ino
+++ electronics_light_sensor/electronics_light_sensor.ino
@@ -2,16 +2,15 @@
 
 Servo myservo;  // create Servo object to control a servo
 // twelve Servo objects can be created on most boards
-
+//array Values = [180,0,90]
 int pos = 0;    // variable to store the servo position
-void move_to(int degree, int wait = 15) { // new function to add delay and move the panel without need to write a for loop every time
+void move_to(int degree, int wait = 1.83333333333) { // new function to add delay and move the panel without need to write a for loop every time
   int change_in_degrees = degree - myservo.read();
   for (int pos = 0; pos <= change_in_degrees; pos +=change_in_degrees/abs(change_in_degrees) ) {
     myservo.write(pos);
     delay(wait);
   }
 }
-// holy chatgpt
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
 }
